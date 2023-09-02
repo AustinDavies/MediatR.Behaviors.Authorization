9c20da2ff91ebd4555d2524e51d2a02585eff487
Austin Davies <austin.davies@swatstudios.com>
2023-09-01 21:25:18 -0500

    Added flexibility to have control over how unauthorized scenarios are handled.

---
+++ MediatR.Behaviors.Authorization/Configuration/AuthorizationPipelineHandlerOptions.cs
@@ -0,0 +1,16 @@
+﻿using MediatR.Behaviors.Authorization.Interfaces;
+
+namespace MediatR.Behaviors.Authorization.Configuration
+{
+    public class AuthorizationPipelineHandlerOptions
+    {
+        public IUnauthorizedResultHandler OnUnauthorized { get; private set; } = new DefaultUnauthorizedHandlerStrategy();
+
+        public AuthorizationPipelineHandlerOptions UseUnauthorizedResultHandlerStrategy(IUnauthorizedResultHandler strategy)
+        {
+            OnUnauthorized = strategy;
+
+            return this;
+        }
+    }
+}
---
+++ MediatR.Behaviors.Authorization/Configuration/DefaultUnauthorizedHandlerStrategy.cs
@@ -0,0 +1,12 @@
+﻿using MediatR.Behaviors.Authorization.Exceptions;
+using MediatR.Behaviors.Authorization.Interfaces;
+using System.Threading.Tasks;
+
+namespace MediatR.Behaviors.Authorization.Configuration
+{
+    internal class DefaultUnauthorizedHandlerStrategy : IUnauthorizedResultHandler
+    {            
+        public Task<TResponse> Invoke<TResponse>(AuthorizationResult result)
+            => throw new UnauthorizedException(result.FailureMessage);
+    }
+}
